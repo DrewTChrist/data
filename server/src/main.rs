f3d63467a237d9e26b3fa798fce3281c0dcaae93
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2023-04-24 15:26:41 -0700

    Add environment variables

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,15 @@
 use actix_cors::Cors;
-use actix_web::{get, http, middleware, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, http, middleware, web, App, HttpResponse, HttpServer, Responder};
 use env_logger::Env;
 use hardware::manager::{DeviceManager, Message};
 use std::sync::Mutex;
 use systemstat::{Platform, System};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 #[get("/data")]
 async fn data(data: web::Data<ServerState>) -> impl Responder {
     let dev_manager = data.dev_manager.lock().unwrap();
-    if let Message::Data(value) = dev_manager.receiver.recv().unwrap() {
-        HttpResponse::Ok().body(value.to_string())
-    } else {
-        HttpResponse::Ok().body("Error retreiving sensor data")
-    }
+    let Message::Data(value) = dev_manager.receiver.recv().unwrap();
+    HttpResponse::Ok().body(value.to_string())
 }
 
 #[get("/devices")]
@@ -38,42 +21,26 @@ async fn devices() -> impl Responder {
 async fn stats(stat: web::Path<String>) -> impl Responder {
     let sys = System::new();
     match stat.as_str() {
-        "cpu_load" => {
-            match sys.cpu_load_aggregate() {
-                Ok(cpu) => {
-                    let cpu = cpu.done().unwrap();
-                    HttpResponse::Ok().json(cpu)
-                }
-                Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
+        "cpu_load" => match sys.cpu_load_aggregate() {
+            Ok(cpu) => {
+                let cpu = cpu.done().unwrap();
+                HttpResponse::Ok().json(cpu)
             }
+            Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
         },
-        "memory" => {
-            match sys.memory() {
-                Ok(mem) => {
-                    HttpResponse::Ok().json(mem)
-                },
-                Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
-            }
+        "memory" => match sys.memory() {
+            Ok(mem) => HttpResponse::Ok().json(mem),
+            Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
         },
-        "uptime" => {
-            match sys.uptime() {
-                Ok(uptime) => {
-                    HttpResponse::Ok().json(uptime)
-                },
-                Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
-            }
-        }
-        "socket" => {
-            match sys.socket_stats() {
-                Ok(stats) => {
-                    HttpResponse::Ok().json(stats)
-                },
-                Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
-            }
-        }
-        _ => {
-            HttpResponse::BadRequest().body("Error")
+        "uptime" => match sys.uptime() {
+            Ok(uptime) => HttpResponse::Ok().json(uptime),
+            Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
         },
+        "socket" => match sys.socket_stats() {
+            Ok(stats) => HttpResponse::Ok().json(stats),
+            Err(e) => HttpResponse::BadRequest().body(format!("{}", e)),
+        },
+        _ => HttpResponse::BadRequest().body("Error"),
     }
 }
 
@@ -92,7 +59,7 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("http://localhost:5173")
+            .allowed_origin(env!("ALLOWED_ORIGIN"))
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
@@ -101,13 +68,10 @@ async fn main() -> std::io::Result<()> {
             .app_data(state.clone())
             .wrap(middleware::Logger::default())
             .wrap(cors)
-            .service(hello)
-            .service(echo)
             .service(data)
             .service(stats)
-            .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((env!("HOST"), env!("PORT").parse().unwrap()))?
     .run()
     .await
 }
